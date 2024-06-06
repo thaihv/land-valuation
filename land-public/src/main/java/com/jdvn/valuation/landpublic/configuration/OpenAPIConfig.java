fba94b00d575cd7163c94eca2f58de197c95da66
thaihv <thaihv@gmail.com>
2024-06-06 17:07:22 +0700

    swagger ui setting

--- land-public/src/main/java/com/jdvn/valuation/landpublic/configuration/OpenAPIConfig.java
+++ land-public/src/main/java/com/jdvn/valuation/landpublic/configuration/OpenAPIConfig.java
@@ -3,12 +3,19 @@ package com.jdvn.valuation.landpublic.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 
 @Configuration
+@SecurityScheme(
+    name = "basicAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "basic"
+)
 public class OpenAPIConfig {
 
   @Bean
--- land-public/src/main/java/com/jdvn/valuation/landpublic/controller/PropertyInfoController.java
+++ land-public/src/main/java/com/jdvn/valuation/landpublic/controller/PropertyInfoController.java
@@ -11,15 +11,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api")
+@SecurityRequirement(name = "basicAuth")
 @Tag(name = "Valuation Unit Information", description = "Valuation Unit Information Management APIs")
 public class PropertyInfoController {
 
     @GetMapping("/parcel/basic")
+	@Operation(
+			summary = "Retrieve basic info of a parcel", 
+			description = "Get all basic info. The response is a collection of values.",
+//			security = @SecurityRequirement(name = "basicAuth"),
+			tags = {"parcel", "get" })
     public Map<String, String>  getParcelBasicInfo(Model model) {
         HashMap<String, String> info = new HashMap<>();
         info.put("key", "value");
