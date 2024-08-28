077282bd29caf1a9989dc17f49054360d6ba59c8
Hoang Do Minh <gigabyte.rp@gmail.com>
2024-08-28 14:40:29 +0700

    Update LeftBar.jsx

--- lvis-ui/src/components/toolbars/LeftBar.jsx
+++ lvis-ui/src/components/toolbars/LeftBar.jsx
@@ -13,9 +13,12 @@ import {
   MenuItem,
   TextField,
   Button,
+  InputAdornment,
+  FormHelperText,
 } from "@mui/material";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import { Search } from "@mui/icons-material";
 import { useTheme } from "@mui/material";
 import { useTranslation } from "react-i18next";
 
@@ -28,10 +31,28 @@ export default function LeftBar() {
     hasDisabledAnimation: true,
   });
 
+  const [searchInput, setSearchInput] = useState("");
+
+  const [propertyType, setPropertyType] = useState("Parcel");
+  const [order, setOrder] = useState("Parcel Number");
+  const [baunitType, setBAUnitType] = useState("Vientiane");
+  const [month, setMonth] = useState("February");
+
+  const handlePropertyTypeChange = (event) => {
+    setPropertyType(event.target.value);
+  };
+  const handleOrderChange = (event) => {
+    setOrder(event.target.value);
+  };
+  const handleBAUnitTypeChange = (event) => {
+    setBAUnitType(event.target.value);
+  };
+  const handleMonthChange = (event) => {
+    setMonth(event.target.value);
+  };
   function handleOnClick() {
     setExpanded(!isExpanded);
   }
-
   return (
     <Stack
       className="collapsible"
@@ -65,53 +86,117 @@ export default function LeftBar() {
               </Typography>
             </Toolbar>
           </AppBar>
-          <Typography component={"span"}>
-            <Stack direction="column" spacing={2}>
-              <Stack direction="row" justifyContent="space-between">
-                <FormControl variant="standard" sx={{ minWidth: 130 }}>
-                  <InputLabel id="property-type">Property Type:</InputLabel>
-                  <Select
-                    labelId="property-type"
-                    id="property"
-                    label="Property Type"
-                  >
-                    <MenuItem value="Parcel">Parcel</MenuItem>
-                    <MenuItem value="Building">Building</MenuItem>
-                    <MenuItem value="Building Unit">Building Unit</MenuItem>
-                    <MenuItem value="Parcel & Building Unit">
-                      Parcel & Building Unit
-                    </MenuItem>
-                  </Select>
-                </FormControl>
-                <FormControl variant="standard" sx={{ minWidth: 130 }}>
-                  <InputLabel id="order">Order By:</InputLabel>
-                  <Select labelId="order" id="order" label="Order By">
-                    <MenuItem value="Parcel Number">Parcel Number</MenuItem>
-                    <MenuItem value="Address">Address</MenuItem>
-                    <MenuItem value="Owner Name">Owner Name</MenuItem>
-                    <MenuItem value="Street Name">Street Name</MenuItem>
-                  </Select>
-                </FormControl>
-              </Stack>
-              <TextField label="Enter criteria..." variant="standard" />
-              <Box sx={{ display: "flex", justifyContent: "center" }}>
-                <Button
-                  sx={{
-                    backgroundColor: "#f2f2f2",
-                    color: "#333333",
-                    fontSize: "12px",
-                    fontWeight: "bold",
-                    padding: "10px 20px",
-                    borderRadius: "25px",
-                  }}
-                  variant="contained"
-                  size="small"
+          <Stack direction="column" spacing={2} m="1.5rem 1.5rem 1rem 1rem">
+            <Box direction="row" justifyContent="space-between">
+              <FormControl variant="standard" sx={{ minWidth: 130 }}>
+                <InputLabel id="property-type">Property Type</InputLabel>
+                <Select
+                  labelId="property-type"
+                  id="property"
+                  label="Property Type"
+                  value={propertyType}
+                  onChange={handlePropertyTypeChange}
                 >
-                  {t("Search")}
-                </Button>
-              </Box>
-            </Stack>
-          </Typography>
+                  <MenuItem value="Parcel">Parcel</MenuItem>
+                  <MenuItem value="Building">Building</MenuItem>
+                  <MenuItem value="Building Unit">Building Unit</MenuItem>
+                  <MenuItem value="Parcel & Building Unit">
+                    Parcel & Building Unit
+                  </MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl variant="standard" sx={{ minWidth: 130 }}>
+                <InputLabel id="order">Order By</InputLabel>
+                <Select
+                  labelId="order"
+                  id="order"
+                  label="Order By"
+                  value={order}
+                  onChange={handleOrderChange}
+                >
+                  <MenuItem value="Parcel Number">Parcel Number</MenuItem>
+                  <MenuItem value="Address">Address</MenuItem>
+                  <MenuItem value="Owner Name">Owner Name</MenuItem>
+                  <MenuItem value="Street Name">Street Name</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl variant="standard" sx={{ minWidth: 130 }}>
+                <InputLabel id="baunit-type">City</InputLabel>
+                <Select
+                  labelId="baunit"
+                  id="baunit"
+                  label="City"
+                  value={baunitType}
+                  onChange={handleBAUnitTypeChange}
+                >
+                  <MenuItem value="Vientiane">Vientiane</MenuItem>
+                  <MenuItem value="Phonsavan">Phonsavan</MenuItem>
+                  <MenuItem value="Savannakhet">Savannakhet</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl variant="standard" sx={{ minWidth: 130 }}>
+                <InputLabel id="time-type">Month</InputLabel>
+                <Select
+                  labelId="time-type"
+                  id="month"
+                  label="Month"
+                  value={month}
+                  onChange={handleMonthChange}
+                >
+                  <MenuItem value="January">January</MenuItem>
+                  <MenuItem value="February">February</MenuItem>
+                  <MenuItem value="March">March</MenuItem>
+                  <MenuItem value="April">April</MenuItem>
+                  <MenuItem value="May">May</MenuItem>
+                  <MenuItem value="June">June</MenuItem>
+                  <MenuItem value="July">July</MenuItem>
+                  <MenuItem value="August">August</MenuItem>
+                  <MenuItem value="September">September</MenuItem>
+                  <MenuItem value="October">October</MenuItem>
+                  <MenuItem value="November">November</MenuItem>
+                  <MenuItem value="December">December</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+            <TextField
+              label="Search..."
+              onChange={(e) => setSearchInput(e.target.value)}
+              value={searchInput}
+              sx={{ mb: "0.5rem" }}
+              variant="standard"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={() => {
+                        console.log(searchInput);
+                        setSearchInput("");
+                      }}
+                    >
+                      <Search />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <FormHelperText>Select criteria to filter</FormHelperText>
+            <Box sx={{ display: "flex", justifyContent: "center" }}>
+              <Button
+                sx={{
+                  backgroundColor: "#f2f2f2",
+                  color: "#333333",
+                  fontSize: "12px",
+                  fontWeight: "bold",
+                  padding: "10px 20px",
+                  borderRadius: "25px",
+                }}
+                variant="contained"
+                size="small"
+              >
+                {t("Search")}
+              </Button>
+            </Box>
+          </Stack>
         </div>
       </div>
       <Box sx={{ position: "absolute", top: "50%", right: "-15px" }}>
