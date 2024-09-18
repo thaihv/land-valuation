379be7e99f6239868aef6f1edc46cc8ad77939e9
Hoang Do Minh <gigabyte.rp@gmail.com>
2024-09-18 17:40:16 +0700

    create docker file

--- stages/lvis-ui0/vite.config.js
+++ stages/lvis-ui0/vite.config.js
@@ -31,4 +31,12 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  }  
 });
