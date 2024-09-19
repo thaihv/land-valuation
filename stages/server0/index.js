7e336df009f3ca43547e7e4923efc78785657bdb
Hoang Do Minh <gigabyte.rp@gmail.com>
2024-09-19 09:16:29 +0700

    update json

--- stages/server0/index.js
+++ stages/server0/index.js
@@ -95,11 +95,11 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /* ONLY ADD DATA ONE TIME */
-    AffiliateStat.insertMany(dataAffiliateStat);
-    OverallStat.insertMany(dataOverallStat);
-    Product.insertMany(dataProduct);
-    ProductStat.insertMany(dataProductStat);
-    Transaction.insertMany(dataTransaction);
-    User.insertMany(dataUser);
+    // AffiliateStat.insertMany(dataAffiliateStat);
+    // OverallStat.insertMany(dataOverallStat);
+    // Product.insertMany(dataProduct);
+    // ProductStat.insertMany(dataProductStat);
+    // Transaction.insertMany(dataTransaction);
+    // User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
