07477702040b4d43869510df8597404409260da1
Hoang Do Minh <gigabyte.rp@gmail.com>
2024-09-23 17:17:55 +0700

    add new page survey without layout

--- stages/dev0/lvis-ui/src/scenes/maps/survey/index.jsx
+++ stages/dev0/lvis-ui/src/scenes/maps/survey/index.jsx
@@ -1,27 +1,43 @@
-import { Box } from "@mui/material";
+import React, { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
 import LeftBar from "../../../components/toolbars/LeftBar";
 import LeafletMap from "../../../components/map/LeafletMap";
+import Sidebar from "../../../components/Sidebar";
 
 const SurveyMap = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const data = useSelector((state) => state.global.user);
+
   return (
-    <Box>
-      <Box
-        height="100vh"
-        position= "relative"
-        sx={{
-          display: "grid",
-          gridTemplateColumns: "repeat(4, 1fr)",
-          gap: 0,
-          gridTemplateRows: "repeat(2, 1fr)",
-          gridTemplateAreas: `"left map map map"
-          "left map map map"`,
-        }}
-      >
-        <Box sx={{ gridArea: "left" }}>
-          <LeftBar />
-        </Box>
-        <Box sx={{ gridArea: "1/1/3/5" }}>
-          <LeafletMap />
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        user={data || {}}
+        isNonMobile={isNonMobile}
+        drawerWidth="88px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box flexGrow={1}>
+        <Box
+          height="100vh"
+          position= "relative"
+          sx={{
+            display: "grid",
+            gridTemplateColumns: "repeat(4, 1fr)",
+            gap: 0,
+            gridTemplateRows: "repeat(2, 1fr)",
+            gridTemplateAreas: `"left map map map"
+            "left map map map"`,
+          }}
+        >
+          <Box sx={{ gridArea: "left" }}>
+            <LeftBar />
+          </Box>
+          <Box sx={{ gridArea: "1/1/3/5" }}>
+            <LeafletMap />
+          </Box>
         </Box>
       </Box>
     </Box>
