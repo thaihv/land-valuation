276c8fa3f95557ca6874160e49fbf3fca8c61e17
Hoang Do Minh <gigabyte.rp@gmail.com>
2024-09-25 15:01:48 +0700

    format code

--- stages/dev0/lvis-ui/src/components/map/DrawMap.jsx
+++ stages/dev0/lvis-ui/src/components/map/DrawMap.jsx
@@ -5,33 +5,30 @@ import osm from "./osm-providers";
 import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
 
-
 const DrawMap = () => {
   const [center, setCenter] = useState({ lat: 19.8563, lng: 102.4955 });
   const ZOOM_LEVEL = 12;
   const _created = (e) => console.log(e);
   return (
     <MapContainer center={center} zoom={ZOOM_LEVEL} className="map">
-    <FeatureGroup>
-      <EditControl
-        position="topright"
-        onCreated={_created}
-        draw={
-          {
+      <FeatureGroup>
+        <EditControl
+          position="topright"
+          onCreated={_created}
+          draw={{
             rectangle: false,
             circle: false,
             circlemarker: false,
             marker: true,
             polyline: true,
-          }
-        }
+          }}
+        />
+      </FeatureGroup>
+      <TileLayer
+        url={osm.maptiler.url}
+        attribution={osm.maptiler.attribution}
       />
-    </FeatureGroup>
-    <TileLayer
-      url={osm.maptiler.url}
-      attribution={osm.maptiler.attribution}
-    />
-  </MapContainer>
+    </MapContainer>
   );
 };
 
