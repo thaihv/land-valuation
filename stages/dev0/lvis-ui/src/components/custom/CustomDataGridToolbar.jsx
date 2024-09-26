65a354d81cec99f3247ccce3dcafc0185e060cf0
Hoang Do Minh <gigabyte.rp@gmail.com>
2024-09-26 12:54:22 +0700

    Update CustomDataGridToolbar.jsx

--- stages/dev0/lvis-ui/src/components/custom/CustomDataGridToolbar.jsx
+++ stages/dev0/lvis-ui/src/components/custom/CustomDataGridToolbar.jsx
@@ -1,16 +1,50 @@
 import React from "react";
 import { Search } from "@mui/icons-material";
-import { IconButton, TextField, InputAdornment } from "@mui/material";
+import { IconButton, Button, TextField, InputAdornment } from "@mui/material";
 import {
   GridToolbarDensitySelector,
   GridToolbarContainer,
   GridToolbarExport,
   GridToolbarColumnsButton,
-  GridToolbarFilterButton
+  GridToolbarFilterButton,
+  gridExpandedSortedRowIdsSelector,
+  gridPaginatedVisibleSortedGridRowIdsSelector,
+  gridSortedRowIdsSelector,
+  useGridApiContext,
 } from "@mui/x-data-grid";
 import FlexBetween from "../FlexBetween";
+import { createSvgIcon } from "@mui/material/utils";
+
+const getRowsFromCurrentPage = ({ apiRef }) =>
+  gridPaginatedVisibleSortedGridRowIdsSelector(apiRef);
+
+const getUnfilteredRows = ({ apiRef }) => gridSortedRowIdsSelector(apiRef);
+
+const getFilteredRows = ({ apiRef }) =>
+  gridExpandedSortedRowIdsSelector(apiRef);
+
+const ExportIcon = createSvgIcon(
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#997d3d"
+  >
+    <path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z" />
+  </svg>
+);
 
 const CustomDataGridToolbar = ({ searchInput, setSearchInput, setSearch }) => {
+  const apiRef = useGridApiContext();
+  const handleExport = (options) => apiRef.current.exportDataAsCsv(options);
+
+  const buttonBaseProps = {
+    color: "primary",
+    size: "small",
+    startIcon: <ExportIcon />,
+  };
+
   return (
     <GridToolbarContainer>
       <FlexBetween width="100%">
@@ -19,6 +53,12 @@ const CustomDataGridToolbar = ({ searchInput, setSearchInput, setSearch }) => {
           <GridToolbarDensitySelector />
           <GridToolbarFilterButton />
           <GridToolbarExport />
+          <Button
+            {...buttonBaseProps}
+            onClick={() => handleExport({ getRowsToExport: getFilteredRows })}
+          >
+            Export Filtered
+          </Button>
         </FlexBetween>
         <TextField
           label="Search..."
@@ -27,7 +67,7 @@ const CustomDataGridToolbar = ({ searchInput, setSearchInput, setSearch }) => {
           value={searchInput}
           variant="standard"
           slotProps={{
-            input:{
+            input: {
               endAdornment: (
                 <InputAdornment position="end">
                   <IconButton
@@ -39,7 +79,7 @@ const CustomDataGridToolbar = ({ searchInput, setSearchInput, setSearch }) => {
                     <Search />
                   </IconButton>
                 </InputAdornment>
-              ),              
+              ),
             },
           }}
         />
