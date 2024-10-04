92f9105c2a6254c8fd73e62fa3e0af75a7a1988e
Hoang Do Minh <thaihv@gmail.com>
2024-10-04 07:42:46 +0700

    Update index.jsx

--- stages/dev0/lvis-ui/src/scenes/pagenotfound/index.jsx
+++ stages/dev0/lvis-ui/src/scenes/pagenotfound/index.jsx
@@ -17,7 +17,7 @@ const PageNotFound = () => {
       }}
     >
       <Container maxWidth="md">
-        <Grid container spacing={2}>
+        <Grid container spacing={5}>
           <Grid xs={4}>
             <img
               src="./404.png"
@@ -29,10 +29,10 @@ const PageNotFound = () => {
             <Typography variant="h4" fontWeight="bold">
               Land Valuation Information System
             </Typography>
-            <Typography variant="h5" color={theme.palette.redAccent[400]}> 
+            <Typography sx={{mt: "5px"}} variant="h5" color={theme.palette.redAccent[400]}> 
               The page you’re looking for doesn’t exist.
             </Typography>
-            <StyledButton sx={{border: `2px solid ${theme.palette.neutral.dark}`}} variant="outlined" onClick={() => navigate("/home")}>
+            <StyledButton sx={{mt: "50px", border: `2px solid ${theme.palette.neutral.dark}`}} variant="outlined" onClick={() => navigate("/home")}>
               Go to Home Page
             </StyledButton>
           </Grid>          
